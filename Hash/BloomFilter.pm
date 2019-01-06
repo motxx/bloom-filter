203500d0ecd975a6a6407205b7433f079440a5db
motxx <motohiko.ave@gmail.com>
2019-01-06 20:53:09 +0900

    wip. try to use murmur128_x86 or smtg else

--- Hash/BloomFilter.pm
+++ Hash/BloomFilter.pm
@@ -1,22 +1,40 @@
 package Hash::BloomFilter;
 
-use Digest::MurmurHash;
+use Digest::MurmurHash3 qw( murmur128_x86 );
 use Carp qw(croak);
+
+use constant BSIZE => 64;
 
 sub new {
     my ($class, %params) = @_;
     my $self = {
-        m => $params{m} // 1000,
+        m => $params{m} // 100000,
         k => $params{k} // 5,
         b => [],
     };
     bless $self, $class;
 }
 
-sub _mapped_index {
+sub _index {
     my ($self, $string) = @_;
-    my $digest = Digest::MurmurHash::murmur_hash($string);
+    my ($d1, $d2) = murmur128_x86($string); # FIXME: 巨大な入力に耐えられるような実装があると旨味が出る
     return $digest % $self->{m};
+}
+
+sub _set_bit {
+    my ($self, $index) = @_;
+    my $bidx = $index / BSIZE;
+    my $eidx = $index % BSIZE;
+    $self->{b}->[$bidx] //= 0;
+    $self->{b}->[$bidx] |= 1 << $eidx;
+}
+
+sub _get_bit {
+    my ($self, $index) = @_;
+    my $bidx = $index / BSIZE;
+    my $eidx = $index % BSIZE;
+    $self->{b}->[$bidx] //= 0;
+    return ($self->{b}->[$bidx] & (1 << $eidx)) > 0;
 }
 
 sub add {
@@ -25,14 +43,16 @@ sub add {
         croak "'m' should be positive.";
     }
     for (my $i = 0; $i < $self->{k}; ++$i) {
-        $self->{b}->[$self->_mapped_index($string . $i)] = 1;
+        $self->_set_bit($self->_index($string . $i));
     }
 }
 
 sub probably_exists {
     my ($self, $string) = @_;
     for (my $i = 0; $i < $self->{k}; ++$i) {
-        unless ($self->{b}->[$self->_mapped_index($string . $i)]) {
+        my $gb = $self->_index($string . $i);
+        print("index: $gb\n");
+        unless ($self->_get_bit($gb)) {
             return 0;
         }
     }
